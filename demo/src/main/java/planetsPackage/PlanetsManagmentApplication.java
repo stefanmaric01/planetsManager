fb40635714979e03a23ea0bb0ec5d52a6183dcdf
stefanmaric01 <stefanmaric01@gmail.com>
2024-04-15 14:11:49 +0200

    First commit

---
+++ demo/src/main/java/planetsPackage/PlanetsManagmentApplication.java
@@ -0,0 +1,13 @@
+package planetsPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsManagmentApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/planetsPackage/PlanetsManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package planetsPackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetsManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
